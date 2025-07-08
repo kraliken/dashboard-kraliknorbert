4a63465dc5a103e07eb8fbd0ec87d6870f4d790e
Kralik N <kraaliknorbert@gmail.com>
2025-07-09 01:17:58 +0200

    separate dashboard layout with conditional Navbar

--- app/layout.js
+++ app/layout.js
@@ -30,7 +30,6 @@ export default function RootLayout({ children }) {
           enableSystem
           disableTransitionOnChange
         >
-          <Navbar />
           {children}
         </ThemeProvider>
       </body>
